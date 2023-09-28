13cef189b828cb4a57dd838c549ed0ef2a82c6e7
Rakshit Gupta <95240061+Rakshitgupta9@users.noreply.github.com>
2023-09-28 14:24:18 +0530

    Add files via upload

---
+++ Activity 1/Activity 1.py
@@ -0,0 +1,31 @@
+#abs
+a=-9
+print("Absolute value of ",a," = ",abs(a))
+
+#len
+b="Rakshit"
+print("Length of ",b," = ",len(b))
+
+#min
+c1=78
+c2=88
+print ("Minimum number between", c1, "and", c2 ,"= :", min (c1,c2) )
+
+#round
+d=24.59
+print("Round off of ",d," = ",round(d))
+
+#isalnum
+e="rgupta9"
+e1=e.isalnum()
+if e1==True:
+    print(e,"is alphanumeric")
+else:
+    print(e,"is not alphanumeric")
+
+#type
+f=5+10j
+print("type of ",f," = ",type(f))
+
+
+
